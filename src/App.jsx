297fd66e73e671267ea8380b820bff2fe665f962
Shauvik Paul <paul99shauvik108@gmail.com>
2024-01-16 09:01:58 +0530

    Zepto Assignment

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useRef, useState } from 'react'
+import './scss/main.scss'
+import { minidenticon } from 'minidenticons'
+import data from './data';
+
+
+
+
+
+const App = () => {
+
+    const inputRef = useRef()
+    const [focus, setFocus] = useState(false)
+    const [datas, setDatas] = useState(data)
+    const [searchBox , setSearchBox] = useState(datas)
+    const [inpNames,setInpNames] = useState([])
+
+
+    const addNames = (ind) => {
+      // console.log(ind)
+      setInpNames(preState => [...preState,datas[datas.findIndex(e => e.id === ind)]])
+      setDatas(datas.filter((el,index)=> el.id !== ind))
+      setSearchBox(datas.filter((el,index)=> el.id !== ind))
+      setFocus(false)
+      inputRef.current.value = ''
+    }
+    
+    const delName = (ind) =>{
+      // console.log(ind)
+      setInpNames(inpNames.filter((el,index) =>  el.id !== ind))
+      setDatas(pre => [...pre , inpNames[inpNames.findIndex(e => e.id === ind)]])
+      setSearchBox(pre => [...pre, inpNames[inpNames.findIndex(e => e.id === ind)]])
+      inputRef.current.value = ''
+    }
+
+    const setNameChange = (e) =>{
+
+        let matches = datas.filter((el)=>{
+            const reg = new RegExp(`${e}`,'gi')
+            return el.name.match(reg)
+        })
+        setSearchBox(matches)
+
+    }
+
+
+    return (
+      <>
+        <div className="outerDiv">
+          <div className="inputDiv">
+            <ul>
+              {
+                inpNames.map((el,ind) => {
+                  return (
+                    <li key={el.id}> <span className='avt'> <img src={`data:image/svg+xml;utf8,${encodeURIComponent(minidenticon(`${el.name}`))}`} alt="avatar" /></span> <span className='name'> {el.name} </span> &nbsp; <span className='closeBtn' onClick={e => delName(el.id)}> x</span></li>
+                  )
+                })
+              }
+            
+            </ul>
+            <input type="text" ref={inputRef} name="autocomplete" placeholder='Write a name' onChange={e => setNameChange(e.target.value)} onFocus={e =>  setFocus(true)} />
+          </div>
+          {
+              focus ? (
+                  <div className="list">
+                    <ul> 
+                    {
+                        searchBox && searchBox.map((el,ind) => {
+                          return (
+                            <li key={el.id} onClick={e => addNames(el.id)}> <span className='avt'> <img src={`data:image/svg+xml;utf8,${encodeURIComponent(minidenticon(`${el.name}`))}`} alt="avatar" /></span> <span className='name'> {el.name} </span> &nbsp; <span className='email' > {el.email}</span></li>
+                          )
+                        })
+                      }
+                    
+                    </ul>
+                  </div>   
+
+              ) : (
+                null
+              )
+          }
+        </div>
+      </>
+    )  
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        id : 1,
+        name : 'John Smith',
+        email : 'j.smith@example.com'
+    },
+    {
+        id : 2,
+        name : 'Arthur Shelby',
+        email : 'a.shelby@example.com'
+    },
+    {
+        id : 3,
+        name : 'David White',
+        email : 'd.white@example.com'
+    },
+    {
+        id : 4,
+        name : 'Albert Eller',
+        email : 'a.eller@example.com'
+    },
+    {
+        id : 5,
+        name : 'Bleak Kernel',
+        email : 'b.kernel@example.com'
+    },
+    {
+        id : 6,
+        name : 'Canary Dumbledore',
+        email : 'c.dumbledore@example.com'
+    },
+    {
+        id : 7,
+        name : 'Dwyane Johnson',
+        email : 'd.johnson@example.com'
+    },
+    {
+        id : 8,
+        name : 'Elizabeth Gray',
+        email : 'e.gray@example.com'
+    },
+    {
+        id : 9,
+        name : 'Fracis Barne',
+        email : 'f.barne@example.com'
+    },
+    {
+        id : 10,
+        name : 'Thomas Shelby',
+        email : 't.shelby@example.com'
+    },
+]
+
+export default data
